63547fe2c42c984fe4f3230fb637dd6585b466d5
Brandon Carl <bcarl@me.com>
2015-10-18 02:17:52 -0700

    add dev.bcarl.me vhost

--- modules/my_sites/manifests/init.pp
+++ modules/my_sites/manifests/init.pp
@@ -1,4 +1,5 @@
 class my_sites {
   my_nginx::site::static { '_': html => '<!DOCTYPE html>', default_server => true }
-  my_nginx::site::static { 'vbzb.com': html => '<!DOCTYPE html><html><body>hello world' }
+  my_nginx::site::static { 'vbzb.com': html => '<!DOCTYPE html><html><body>vbzb.com' }
+  my_nginx::site::static { 'dev.bcarl.me': html => '<!DOCTYPE html><html><body>dev.bcarl.me' }
 }
