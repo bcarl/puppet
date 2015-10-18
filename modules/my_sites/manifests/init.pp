4381e2c49d36a2fb1132961b0b46418a8b14ef7d
Brandon Carl <bcarl@me.com>
2015-10-18 02:16:45 -0700

    update site html

--- modules/my_sites/manifests/init.pp
+++ modules/my_sites/manifests/init.pp
@@ -1,4 +1,4 @@
 class my_sites {
-  my_nginx::site::static { '_': html => 'default', default_server => true }
-  my_nginx::site::static { 'vbzb.com': html => 'hello world' }
+  my_nginx::site::static { '_': html => '<!DOCTYPE html>', default_server => true }
+  my_nginx::site::static { 'vbzb.com': html => '<!DOCTYPE html><html><body>hello world' }
 }
