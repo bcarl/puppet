374d0810507e222b009b865bbd7d62d51ee41b27
Brandon Carl <bcarl@me.com>
2015-10-19 00:34:53 -0700

    decisions, decisions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -e
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-a-masterless-puppet-environment-on-ubuntu-14-04
 
-## Install Git
+# install git and puppet
 apt-get update
-apt-get -y install git-core
+apt-get -y install git-core puppet
 
-# Clone the 'puppet' repo
-[ -e /etc/puppet ] && mv /etc/puppet /etc/puppet-bak
-git clone "https://github.com/bcarl/puppet.git" /etc/puppet
-
-# Install and disable Puppet
-apt-get -y install puppet
+# disable puppet
+puppet agent --disable
 killall puppet
 systemctl disable puppet.service
 
-# Install Puppet modules
+# clone the 'puppet' repo
+[ -e /etc/puppet ] && mv /etc/puppet /etc/puppet-bak
+git clone "https://github.com/bcarl/puppet.git" /etc/puppet
+
+# install puppet modules
 for module in jfryman-nginx puppetlabs-firewall puppetlabs-ntp puppetlabs-stdlib puppetlabs-vcsrepo saz-ssh saz-sudo; do
   puppet module install $module
 done
 
-# Run Puppet initially to set up the auto-deploy mechanism
+# run puppet initially to set up the auto-deploy mechanism
 puppet apply /etc/puppet/manifests/site.pp
