2dbd8d7da93d10d52a7d182b97153c5b33b8cf68
Brandon Carl <bcarl@me.com>
2015-10-19 01:08:55 -0700

    add u9t.net to sites

--- modules/my_sites/manifests/init.pp
+++ modules/my_sites/manifests/init.pp
@@ -1,6 +1,7 @@
 class my_sites {
   my_nginx::site::static { '_': default_server => true, content => '<!DOCTYPE html>' }
   my_nginx::site::static { 'spleeyah.com': source => 'puppet:///modules/my_sites/empty.html' }
+  my_nginx::site::static { 'u9t.net': source => 'puppet:///modules/my_sites/empty.html'}
   my_nginx::site::static { 'vbzb.com': source => 'puppet:///modules/my_sites/empty.html'}
   my_nginx::site::jekyll { 'jekyll-test.bcarl.me':
     repo => 'https://github.com/bcarl/bcarl.me.git',
