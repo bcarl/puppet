f8e0996c50b65d9ca1e42e1b372e9c48617fdc62
Brandon Carl <bcarl@me.com>
2015-10-19 00:23:46 -0700

    reorg bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -e
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-a-masterless-puppet-environment-on-ubuntu-14-04
 
-## Install Git and Puppet
+## Install Git
 apt-get update
-apt-get -y install git-core puppet
-
-killall puppet
-systemctl disable puppet.service
+apt-get -y install git-core
 
 # Clone the 'puppet' repo
 [ -e /etc/puppet ] && mv /etc/puppet /etc/puppet-bak
 git clone "https://github.com/bcarl/puppet.git" /etc/puppet
+
+# Install and disable Puppet
+apt-get -y install puppet
+killall puppet
+systemctl disable puppet.service
 
 # Install Puppet modules
 for module in jfryman-nginx puppetlabs-firewall puppetlabs-ntp puppetlabs-stdlib puppetlabs-vcsrepo saz-ssh saz-sudo; do
