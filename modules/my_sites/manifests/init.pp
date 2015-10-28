bf11fff8e586b3b84fde77eec96715161f00b286
Brandon Carl <bcarl@me.com>
2015-10-27 22:36:46 -0700

    Add samlinmusic.com

--- modules/my_sites/manifests/init.pp
+++ modules/my_sites/manifests/init.pp
@@ -1,5 +1,6 @@
 class my_sites {
   my_nginx::site::static { '_': default_server => true, content => '<!DOCTYPE html>' }
+  my_nginx::site::static { 'samlinmusic.com': source => 'puppet:///modules/my_sites/empty.html' }
   my_nginx::site::static { 'spleeyah.com': source => 'puppet:///modules/my_sites/empty.html' }
   my_nginx::site::static { 'u9t.net': source => 'puppet:///modules/my_sites/empty.html'}
   my_nginx::site::static { 'vbzb.com': source => 'puppet:///modules/my_sites/empty.html'}
