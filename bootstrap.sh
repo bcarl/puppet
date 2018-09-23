3ed439c9d8f0a16024b8743be42288a9c5e6111b
Brandon Carl <bcarl@me.com>
2018-09-23 04:26:25 -0400

    Fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-a-masterless-puppet-environment-on-ubuntu-14-04
 
-# install git and puppet
+# install git
 apt-get update
-apt-get -y install git-core puppet
+apt-get -y install git-core
+
+# install puppet
+wget https://apt.puppetlabs.com/puppet-release-$RELEASE.deb
+dpkg -i puppet-release-$RELEASE.deb
 
 # disable puppet
 puppet agent --disable
